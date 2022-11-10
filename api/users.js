7a91dcb6c99857fecc0c05c6f8fd6a64a00277d2
Alex <alexwinston2123@icloud.com>
2022-11-10 13:29:52 -0600

    register router working

--- api/users.js
+++ api/users.js
@@ -1,12 +1,40 @@
 const express = require('express');
-const router = express.Router();
+const { getUserByUsername, createUser } = require('../db');
+const usersRouter = express.Router();
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
 
 // POST /api/users/login
 
 // POST /api/users/register
+usersRouter.post("/register",async (req, res, next) =>{
+	const { username, password } = req.body;
+	if(password.length < 8){
+		next({
+			name:"PasswordLengthError",
+			message:"Password Too Short!"});
+	}
+	try {
+		const _user = await getUserByUsername(username);
+		if( _user ){
+			next({
+				name: "UserExistError",
+				message: `User ${username} is already taken.`,
+			})
+		}else {
+		console.log(username)
+		const user = await createUser({username, password});
+		const token = jwt.sign({ id: user.id, username }, JWT_SECRET, {
+			expiresIn: "1w",
+		});
+		res.send({ message: "Thank you for signing up!", token, user})}
+	} catch({ name, message }) {
+		next({ name, message });
+	}
+})
 
 // GET /api/users/me
 
 // GET /api/users/:username/routines
 
-module.exports = router;
+module.exports = usersRouter;
--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const app = express()
 // Setup your Middleware and API Router here
 
 const cors = require("cors")
-
 app.use(cors());
+app.use(express.json())
 app.use('/api', router)
 
 module.exports = app;
