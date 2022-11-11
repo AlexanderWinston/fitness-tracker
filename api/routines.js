ea371ab3f88f315662f1c5ba2130d2577c58af42
Alex <alexwinston2123@icloud.com>
2022-11-11 13:17:17 -0600

    patch & delete routines router working

--- api/routines.js
+++ api/routines.js
@@ -1,75 +1,105 @@
-const express = require('express');
-const { getAllPublicRoutines, createRoutine, getUserById, getRoutineById, updateRoutine,  } = require('../db');
-const { requireUser} = require('./utils')
+const express = require("express");
+const {
+  getAllPublicRoutines,
+  createRoutine,
+  getUserById,
+  getRoutineById,
+  updateRoutine,
+  destroyRoutine,
+} = require("../db");
+const { requireUser } = require("./utils");
 const routinesRouter = express.Router();
 
 // GET /api/routines
-routinesRouter.get('/', async (req, res) =>{
-	const allRoutines = await getAllPublicRoutines();
-	console.log(allRoutines)
-	res.send(allRoutines)
-})
+routinesRouter.get("/", async (req, res) => {
+  const allRoutines = await getAllPublicRoutines();
+  console.log(allRoutines);
+  res.send(allRoutines);
+});
 
 // POST /api/routines
-routinesRouter.post('/', requireUser, async (req, res, next) =>{
-	const {  creatorId, isPublic, name, goal } = req.body;
-	const routineData = {creatorId, isPublic, name, goal};
-	try {
-		const routines = await createRoutine(routineData)
-		console.log(routines)
-		// if (creatorId == req.user.id){
-		res.send({
-			creatorId:req.user.id, 
-			isPublic,
-			name,
-			goal
-		})
-	}catch({name, message}){
-			next({name, message})
-
-
-		}
-	
-})
+routinesRouter.post("/", requireUser, async (req, res, next) => {
+  const { creatorId, isPublic, name, goal } = req.body;
+  const routineData = { creatorId, isPublic, name, goal };
+  try {
+    const routines = await createRoutine(routineData);
+    console.log(routines);
+    // if (creatorId == req.user.id){
+    res.send({
+      creatorId: req.user.id,
+      isPublic,
+      name,
+      goal,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
+});
 
 // PATCH /api/routines/:routineId
-routinesRouter.patch('/:routineId', requireUser, async (req, res, next)=> {
-	const{routineId} =req.params
-	const {creatorId, isPublic, name, goal } = req.body
-	const updateFields = {}
+routinesRouter.patch("/:routineId", requireUser, async (req, res, next) => {
+  try {
+    const routineId = req.params.routineId;
+    const originalRoutine = await getRoutineById(routineId);
+    if (originalRoutine) {
+      if (originalRoutine.creatorId === req.user.id) {
+        const fields = req.body;
 
-	if (isPublic){
-		updateFields.isPublic=isPublic
-	}
-	if (name){
-		updateFields.name=name
-	}
-	if (goal){
-		updateFields.goal=goal
-	}
-	try {
-		const originalRoutine = await getRoutineById(routineId)
-		console.log(originalRoutine, "original Routine!!")
-		if(originalRoutine.creatorId === req.user.id){
-			const updatedRoutine = await updateRoutine(routineId, updateFields)
-			res.send({routine:updatedRoutine})
-		} else {
-			
-				res.statusCode= 403
-				next({
-				name: "UnauthorizedUserError",
-				message: "You cannot update a routine that is not yours"
-			})
-		
-		}
+        const updatedRoutine = await updateRoutine({
+          id: routineId,
+          ...fields,
+        });
 
-	} catch({name,message}){
-		next({name,message})
-	}
-	
-})
+        res.send(updatedRoutine);
+      } else {
+        res.statusCode = 403;
+        next({
+          error: "403Error",
+          name: "UnauthorizedUserError",
+          message: `User ${req.user.username} is not allowed to update ${originalRoutine.name}`,
+        });
+      }
+    } else {
+      next({
+        error: "noRoutine",
+        name: "Does not exist",
+        message: `${routineId} does not exist`,
+      });
+    }
+  } catch ({ error, name, message }) {
+    next({ error, name, message });
+  }
+});
 
 // DELETE /api/routines/:routineId
+routinesRouter.delete("/:routineId", requireUser, async (req, res, next) => {
+  try {
+    const routineId = req.params.routineId;
+    const routine = await getRoutineById(routineId);
+    if (routine) {
+      if (routineId && routine.creatorId === req.user.id) {
+        const destroyedRoutine = await destroyRoutine(routineId);
+		console.log(destroyedRoutine, 'this is destroy routine')
+        res.send(destroyedRoutine);
+      } else {
+        res.statusCode = 403;
+        next({
+          error: "deleteError",
+          name: "not authorized user",
+          message: `User ${req.user.username} is not allowed to delete ${routine.name}`,
+        });
+      }
+    } else {
+      next({
+        error: "noRoutineError",
+        name: "Does not exist",
+        message: `${routineId} does not exist`,
+      });
+    }
+  } catch ({ error, name, message }) {
+    next({ error, name, message });
+  }
+});
 
 // POST /api/routines/:routineId/activities
 
