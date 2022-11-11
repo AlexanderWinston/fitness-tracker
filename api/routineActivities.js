4710593b2102d0541646d5f84019ef35feb6fb38
Allyson Mitchem <allysonmitchem@gmail.com>
2022-11-11 14:28:53 -0700

    both activities_routines are working in api/passing tests

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,23 +1,64 @@
 const express = require('express');
 const routines_activitiesRouter = express.Router();
 const {requireUser} = require("./utils")
-const {getRoutineActivityById, destroyRoutineActivity, getRoutineById} = require ("../db")
+const {getRoutineActivityById, destroyRoutineActivity, getRoutineById, updateRoutineActivity} = require ("../db")
 
 // PATCH /api/routine_activities/:routineActivityId
+routines_activitiesRouter.patch("/:routineActivityId", requireUser, async (req, res, next)=>{
+    
+    try {
+        const routineActivityId = req.params.routineActivityId;
+       
+        const originalRoutineActivity = await getRoutineActivityById(routineActivityId);
+       
+        const routine = await getRoutineById(originalRoutineActivity.routineId)
+        const fields = req.body;
+       
+        if (originalRoutineActivity) {
+          if (routine.creatorId === req.user.id) {
+           
+    
+            const updatedRoutineActivity = await updateRoutineActivity({
+              id: routineActivityId,
+              ...fields,
+            });
+            
+            
+    
+            res.send(updatedRoutineActivity);
+          } else {
+            res.statusCode = 403;
+            next({
+              error: "403Error",
+              name: "UnauthorizedUserError",
+              message: `User ${req.user.username} is not allowed to update ${routine.name}`,
+            });
+          }
+        } else {
+          next({
+            error: "noRoutine",
+            name: "Does not exist",
+            message: `${routineActivityId} does not exist`,
+          });
+        }
+      } catch ({ error, name, message }) {
+        next({ error, name, message });
+      }
+})
 
 // DELETE /api/routine_activities/:routineActivityId
 routines_activitiesRouter.delete("/:routineActivityId", requireUser, async(req, res, next)=> {
     try {
         const routineActivityId = req.params.routineActivityId;
-        console.log(routineActivityId, " routineActivityId")
+       
         const routineActivity = await getRoutineActivityById(routineActivityId);
-        console.log(routineActivity.name, "this is routineActivity")
+        
         const routine = await getRoutineById(routineActivity.routineId)
-        console.log(routine, " this is routine!")
+       
         if (routineActivity) {
           if (routineActivityId && routine.creatorId === req.user.id) {
             const destroyedRoutineActivity = await destroyRoutineActivity(routineActivityId);
-            console.log(destroyedRoutineActivity, 'this is destroyed routine activity')
+            
             res.send(destroyedRoutineActivity);
           } else {
             res.statusCode = 403;
